720c410fe8b92af664ec7495dbfbabca675d7944
KBaba7 <127428738+KBaba7@users.noreply.github.com>
2025-06-12 23:35:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Experience />
+    </div>
+  );
+}
+
+export default App;
